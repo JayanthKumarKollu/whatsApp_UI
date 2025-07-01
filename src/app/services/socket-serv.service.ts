84906a93f9359bd1626bd34b9635f468aefd7d19
JayanthKumarKollu <99634165+JayanthKumarKollu@users.noreply.github.com>
2025-07-01 15:45:59 +0530

    Update socket-serv.service.ts

--- src/app/services/socket-serv.service.ts
+++ src/app/services/socket-serv.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { concatAll } from 'rxjs';
-import {io,Socket} from 'socket.io-client'
+import {io,Socket} from 'socket.io-client';
+import { environment } from '../../environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
@@ -15,7 +17,7 @@ export class SocketServService {
   constructor() { }
 
   connect(){
-    this.socket=io("http://localhost:3000");
+    this.socket=io(environment.baseUrl);
    
   }
   setuserId(id:any){
